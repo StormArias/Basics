10004e86642a155ac2abccb5fef2b81271adb1ef
Storm Anduaga-Arias <stormarias@gmail.com>
2020-11-27 17:51:55 -0700

    Beginnings

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExerciseTwo;
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne;
+            int[] arrayTwo;
+            int length;
+            bool areEqual;
+
+            Console.Write("Enter first array length: ");
+            length = int.Parse(Console.ReadLine());
+            arrayOne = ArrayMaker(length);
+
+            Console.Write("Enter second array length: ");
+            length = int.Parse(Console.ReadLine());
+            arrayTwo = ArrayMaker(length);
+
+            areEqual = ArrayCompare.CompareArray(arrayOne, arrayTwo);
+            Console.WriteLine($"Array Equivalency = {areEqual}");
+        }
+
+        public static int[] ArrayMaker(int arrayLength)
+        {
+            int[] array = new int[arrayLength];
+            int value;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write($"Enter the number for index {i}: ");
+                value = int.Parse(Console.ReadLine());
+                array[i] = value;
+            }
+
+            Console.WriteLine("Array is complete!");
+            return array;
+        }
+    }
+}
---
+++ Arrays/ExerciseTwo/ArrayCompare.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExerciseTwo
+{
+    // Write a program, which reads two arrays from the console and checks
+    // whether they are equal(two arrays are equal when they are of equal
+    // length and all of their elements, which have the same index, are equal).
+    public class ArrayCompare
+    {
+        public static bool CompareArray(int[] arrayOne, int[] arrayTwo)
+        {
+            bool isEqualLength;
+            bool? hasEqualIndexValues = null;
+
+            isEqualLength = (arrayOne.Length == arrayTwo.Length);
+
+            if (!isEqualLength)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                hasEqualIndexValues = (arrayOne[i] == arrayTwo[i]);
+                if ((bool)!hasEqualIndexValues)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
